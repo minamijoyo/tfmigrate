7140b2ef78ed8b2be6096e86c3fda8fe60917125
Masayuki Morita <minamijoyo@gmail.com>
2020-07-15 23:14:37 +0900

    Add terraform apply command

--- tfexec/terraform.go
+++ tfexec/terraform.go
@@ -33,6 +33,9 @@ type TerraformCLI interface {
 	// Init initializes a given work directory.
 	Init(ctx context.Context, dir string, opts ...string) error
 
+	// Apply applies changes.
+	Apply(ctx context.Context, dirOrPlan string, opts ...string) error
+
 	// StatePull returns the current tfstate from remote.
 	StatePull(ctx context.Context) (State, error)
 
@@ -91,6 +94,17 @@ func (c *terraformCLI) Init(ctx context.Context, dir string, opts ...string) err
 	args = append(args, opts...)
 	if len(dir) > 0 {
 		args = append(args, dir)
+	}
+	_, err := c.run(ctx, args...)
+	return err
+}
+
+// Apply applies changes.
+func (c *terraformCLI) Apply(ctx context.Context, dirOrPlan string, opts ...string) error {
+	args := []string{"apply"}
+	args = append(args, opts...)
+	if len(dirOrPlan) > 0 {
+		args = append(args, dirOrPlan)
 	}
 	_, err := c.run(ctx, args...)
 	return err
