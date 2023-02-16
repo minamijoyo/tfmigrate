e369af224792e93b289be1ebe26378901466b5e1
Masayuki Morita <minamijoyo@gmail.com>
2023-02-16 10:01:20 +0900

    Use a local filesystem mirror on CI
    Starting with Terraform v1.4, the global plugin cache is ignored on the
    first terraform init. This makes caching in CI meaningless.
    https://github.com/hashicorp/terraform/pull/32494
    
    To utilize the cache, we use a local filesystem mirror. Strictly
    speaking, the mirror is only available in Terraform v0.13+, but it is
    hard to compare versions in bash, so we use the mirror unless v0.x.
    https://developer.hashicorp.com/terraform/cli/config/config-file#implied-local-mirror-directories

--- scripts/testacc/generate_plugin_cache.sh
+++ scripts/testacc/generate_plugin_cache.sh
@@ -22,6 +22,27 @@ terraform {
 }
 EOF
 
+# Starting with Terraform v1.4, the global plugin cache is ignored on the first
+# terraform init. This makes caching in CI meaningless. To utilize the cache,
+# we use a local filesystem mirror. Strictly speaking, the mirror is only
+# available in Terraform v0.13+, but it is hard to compare versions in bash,
+# so we use the mirror unless v0.x.
+# https://developer.hashicorp.com/terraform/cli/config/config-file#implied-local-mirror-directories
+if terraform -v | grep 'Terraform v0\.'; then
+  echo "skip creating an implied local mirror"
+else
+  FS_MIRROR="/tmp/plugin-mirror"
+  terraform providers mirror "${FS_MIRROR}"
+
+  cat << EOF > "$HOME/.terraformrc"
+provider_installation {
+  filesystem_mirror {
+    path    = "/tmp/plugin-mirror"
+  }
+}
+EOF
+fi
+
 terraform init -input=false -no-color
 
 popd
